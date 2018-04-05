11a6c513f2cf187e9fc97c8172fefc43862bc949
guchen <805024460@qq.com>
2018-04-05 15:44:54 +0800

    determine interface

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,65 +1,42 @@
 package com.company;
-import org.apache.commons.cli.*;
 
-import java.util.Collections;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
-        Counter counter=Main.commandParser(args);
+        ArrayList<WordInfo> wordInfos=null;
+
+
     }
 
-    public static Counter commandParser(String[] args) {
-        Options opt=new Options();
-        opt.addOption(Option.builder("c").optionalArg(true).hasArgs().build());
-        opt.addOption(Option.builder("w").optionalArg(true).hasArgs().build());
-        opt.addOption(Option.builder("l").optionalArg(true).hasArgs().build());
-        opt.addOption(Option.builder("o").hasArg().build());
-        opt.addOption(Option.builder("s").hasArg(false).build());
-        opt.addOption(Option.builder("a").optionalArg(true).hasArgs().build());
-        opt.addOption(Option.builder("e").hasArg().build());
-        opt.addOption(new Option("h", false, "help"));
-        CommandLineParser parser = new DefaultParser();
-        Counter counter=new Counter();
-        String[] params;
-        try {
-            CommandLine cmd = parser.parse(opt, args);
-            if (cmd.hasOption("c")) {
-                counter.getOptlist().add("c");
-                if ((params=cmd.getOptionValues("c"))!=null)
-                    Collections.addAll(counter.getFilelist(),params);
-            }
-            if (cmd.hasOption("w")) {
-                counter.getOptlist().add("w");
-                if ((params=cmd.getOptionValues("c"))!=null)
-                    Collections.addAll(counter.getFilelist(),params);
-            }
-            if (cmd.hasOption("l")) {
-                counter.getOptlist().add("l");
-                if ((params=cmd.getOptionValues("l"))!=null)
-                    Collections.addAll(counter.getFilelist(),params);
-            }
-            if (cmd.hasOption("o")) {
-                counter.getOptlist().add("o");
-                if ((params=cmd.getOptionValues("o"))!=null)
-                    counter.setOutFileName(params[0]);
-            }
-            if (cmd.hasOption("a")) {
-                counter.getOptlist().add("a");
-                if ((params=cmd.getOptionValues("a"))!=null)
-                    Collections.addAll(counter.getFilelist(),params);
-            }
-            if (cmd.hasOption("e")) {
-                counter.getOptlist().add("e");
-                if ((params=cmd.getOptionValues("e"))!=null)
-                    counter.setStopFileName(params[0]);
-            }
-            if (cmd.hasOption("s")) {
-                counter.getOptlist().add("s");
-            }
-        }catch (ParseException e){
-            System.out.println("Incorrect Command: "+e.getMessage());
-        }
-        return counter;
+    /**
+     * 输入验证
+     * @author
+     * @param args
+     */
+    public void InputValidate(String[] args) {
+
+    }
+
+    /**
+     * 词频统计
+     * @author
+     * @param filename
+     * @return ArrayList<WordInfo>
+     */
+    public ArrayList<WordInfo> countFrequency(String filename) {
+        ArrayList<WordInfo> wordInfos=new ArrayList<>();
+
+        return wordInfos;
+    }
+
+    /**
+     *输出结果
+     * @author
+     * @param wordInfos
+     */
+    public void output(ArrayList<WordInfo> wordInfos){
+
     }
 }
---
+++ src/com/company/WordInfo.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class WordInfo {
+
+    private String word; //单词
+    private int frequency; //词频
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+    }
+
+}
