4809b6e7a4a4c8c2a79d8dc45b7750a8099dfc8f
guchen <805024460@qq.com>
2018-04-07 15:17:14 +0800

    实现词频统计功能

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,19 @@
 package com.company;
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         ArrayList<WordInfo> wordInfos=null;
-
-
+        wordInfos=Main.countFrequency("test.txt");
+        Main.output(wordInfos);
     }
 
     /**
@@ -15,7 +21,7 @@ public class Main {
      * @author
      * @param args
      */
-    public void InputValidate(String[] args) {
+    public static void InputValidate(String[] args) {
 
     }
 
@@ -25,18 +31,60 @@ public class Main {
      * @param filename
      * @return ArrayList<WordInfo>
      */
-    public ArrayList<WordInfo> countFrequency(String filename) {
+    public static ArrayList<WordInfo> countFrequency(String filename) {
         ArrayList<WordInfo> wordInfos=new ArrayList<>();
-
+        Pattern pattern=Pattern.compile("[a-zA-Z]+-?[a-zA-Z]*");
+        String text=Main.readFile(filename);
+        Matcher matcher=pattern.matcher(text);
+        String word;
+        int index;
+        WordInfo wordInfo;
+        while(matcher.find()) {
+            word=matcher.group().toLowerCase();
+            if (word.endsWith("-"))
+                word=word.substring(0,word.length()-1);
+            if ((index=Main.hasWord(wordInfos,word))!=-1) {
+                wordInfos.get(index).setFrequency(wordInfos.get(index).getFrequency()+1);
+            }else{
+                wordInfo=new WordInfo(word, 1);
+                wordInfos.add(wordInfo);
+            }
+        }
         return wordInfos;
     }
 
+    public static int hasWord(ArrayList<WordInfo> wordInfos, String word) {
+        for (WordInfo wordInfo:wordInfos){
+            if (wordInfo.getWord().equals(word.trim().toLowerCase()))
+                return wordInfos.indexOf(wordInfo);
+        }
+        return -1;
+    }
+
+    public static String readFile(String filename) {
+        File file=new File(filename);
+        StringBuilder sb = new StringBuilder();
+        try {
+            FileReader reader = new FileReader(file);
+            BufferedReader br = new BufferedReader(reader);
+            String str = "";
+            while ((str = br.readLine()) != null) { //逐行读取文件内容，不读取换行符和末尾的空格
+                sb.append(str + "\n");
+            }
+            br.close();
+        }catch (IOException e){
+            System.out.println("读取文件失败！");
+        }
+        return sb.toString();
+    }
     /**
      *输出结果
      * @author
      * @param wordInfos
      */
-    public void output(ArrayList<WordInfo> wordInfos){
-
+    public static void output(ArrayList<WordInfo> wordInfos){
+        for (WordInfo e:wordInfos) {
+            System.out.println(e.getWord()+" : "+e.getFrequency());
+        }
     }
 }
--- src/com/company/WordInfo.java
+++ src/com/company/WordInfo.java
@@ -5,6 +5,17 @@ public class WordInfo {
     private String word; //单词
     private int frequency; //词频
 
+    public WordInfo(String word){
+        this.word = word;
+        this.frequency = 0;
+    }
+
+    public WordInfo(String word, int frequency) {
+        this.word = word;
+        this.frequency = frequency;
+    }
+
+
     public String getWord() {
         return word;
     }
